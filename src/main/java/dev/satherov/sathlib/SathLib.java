9a407b59b91f9eb070850839341b7a4eb4bce5b9
Satherov <satherov@users.noreply.github.com>
2026-04-15 21:40:03 +0200

    Initial Commit

---
+++ src/main/java/dev/satherov/sathlib/SathLib.java
@@ -0,0 +1,15 @@
+package dev.satherov.sathlib;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLModContainer;
+
+@Mod(SathLib.MOD_ID)
+public class SathLib {
+    
+    public static final String MOD_ID = "sathlib";
+
+    public SathLib(final IEventBus bus, final FMLModContainer container) {
+
+    }
+}
---
+++ src/main/java/dev/satherov/sathlib/SathLibClient.java
@@ -0,0 +1,17 @@
+package dev.satherov.sathlib;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLModContainer;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = SathLib.MOD_ID, dist = Dist.CLIENT)
+public class SathLibClient {
+    
+    public SathLibClient(final IEventBus bus, final FMLModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+}
