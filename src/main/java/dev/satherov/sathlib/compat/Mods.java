4ab5da5c99212e06441c185efea1a28fa8ba58ac
Satherov <satherov@users.noreply.github.com>
2026-04-30 00:26:00 +0200

    shut up javadoc :3

--- src/main/java/dev/satherov/sathlib/compat/Mods.java
+++ src/main/java/dev/satherov/sathlib/compat/Mods.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@SuppressWarnings("doclint:missing")
 public enum Mods implements CompatMod {
     JADE("jade"),
     JEI("jei"),
